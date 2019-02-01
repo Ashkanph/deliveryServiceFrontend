5b3cb9a625baff5657a66bec1e4a14d329c4b40f
Ashkan Paleganeh <ashkanpaleganeh@gmail.com>
2019-02-01 13:05:22 +0330

    Add setting.js and fonts to the git (It is a bad work to add binary files to the repository, I know)

---
+++ dist/static/setting.js
@@ -0,0 +1,11 @@
+let setting =  {
+    backendMainAddress: 'http://127.0.0.1:3000/',
+    timeDelimeter: ":",
+    dateDelimeter: "/",
+    managerPageRefreshRate: 10000,  // ms
+    bikerPageRefreshRate:   10000,  // ms
+    version: "1.0.0",
+    log:{
+        react: false
+    }
+};
